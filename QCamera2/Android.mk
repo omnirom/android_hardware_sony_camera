01e4531501cf1892b06d9a1847b1ce9972e83bed
Humberto Borba <humberos@gmail.com>
2017-09-12 16:16:10 -0300

    sony: camera: Update include path
    Change-Id: I303602339afc153dcae80066d8f9bd5b8934e062
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- QCamera2/Android.mk
+++ QCamera2/Android.mk
@@ -12,7 +12,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_COPY_HEADERS_TO := qcom/camera
+LOCAL_COPY_HEADERS_TO := sony/camera
 LOCAL_COPY_HEADERS := QCameraFormat.h
 
 LOCAL_SRC_FILES := \
@@ -106,7 +106,7 @@ LOCAL_C_INCLUDES := \
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
-        hardware/qcom/camera/QCamera2/HAL
+        hardware/sony/camera/QCamera2/HAL
 
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
